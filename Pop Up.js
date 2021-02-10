2b5a8f19e470105627f93c6ddda5593025bf441c
laid-sudo <78878013+laid-sudo@users.noreply.github.com>
2021-02-11 00:42:47 +0100

    Add files via upload

---
+++ Pop Up.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="UTF-8">
+      <script> 
+      function showCity(){ alert (villeCourante);}
+      let villeCourante ="Villeurbanne"
+    
+    </script>
+	</head>
+
+	<body  onload="showCity()">
+	  <h1></h1>
+	  <p></p>
+	</body>
+</html>
